8720d89c0722719dfb0d6feeaeecb68633ae4601
D-Rex-Glitch <meranga_akash@outlook.com>
2026-03-15 10:56:41 +0530

    chore: restore default API/server port to 8089

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8087"
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8089"
 });
 
 api.interceptors.request.use((config) => {
